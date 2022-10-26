e5fab48ec839d654cd49fdd26aa4efcf4799a844
amitpaultl <amitpaultl@gmail.com>
2022-10-26 15:55:16 +0600

    Cours title add

--- src/Component/Page/Courses/Courses.js
+++ src/Component/Page/Courses/Courses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import './Courses.css'
 
 const Courses = () => {
@@ -11,21 +11,34 @@ const Courses = () => {
     return (
         <div className='container'>
             <div className="row">
-                {
-                    data.map(course => <div key={course.id} className="col-md-4">
-                        <div onClick={()=>handaler(course.id)} className="course">
-                            <img src={course.picture} alt="" />
-                            <div className="p-4">
-                                <h3>{course.name}</h3>
-                                <div className="d-flex justify-content-between">
-                                    <h4>Price: {course.balance}</h4>
-                                    <h5>Deities</h5>
-                                </div>
-                            </div>
+                <div className="col-md-3">
+                    <div className="course-title">
+                        {
+                           data.map(course => <li className='cou-title' key={course.id}><span onClick={()=>handaler(course.id)}>{course.name}</span></li> ) 
+                        }
+                    </div>
 
-                        </div>
-                    </div>)
-                }
+                </div>
+                <div className="col-md-9">
+                    <div className="row">
+
+                        {
+                            data.map(course => <div key={course.id} className="col-md-4">
+                                <div onClick={()=>handaler(course.id)} className="course">
+                                    <img src={course.picture} alt="" />
+                                    <div className="p-4">
+                                        <h3>{course.name}</h3>
+                                        <div className="d-flex justify-content-between">
+                                            <h4>Price: {course.balance}</h4>
+                                            <h5>Deities</h5>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                </div>
 
             </div>
         </div>
