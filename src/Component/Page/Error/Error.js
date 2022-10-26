01b553601b2096ece41742bbc092dc42de5797f0
amitpaultl <amitpaultl@gmail.com>
2022-10-26 18:00:27 +0600

    Footer add

--- src/Component/Page/Error/Error.js
+++ src/Component/Page/Error/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Error.css'
 
 const Error = () => {
     return (
-        <div>
-            
+        <div >
+            <div className="error-page text-center">
+                <h1>404</h1>
+            </div>
         </div>
     );
 };
